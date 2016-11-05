b6ce581724fd46e74db637c759f0750bbbe9f3c0
Maksim Pinigin <maksim.pinigin@yahoo.com>
2016-11-06 06:32:22 +0700

    changed pinos 0(.1) license

--- pinos.cmd
+++ pinos.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 cd C:\pinos
-echo Pinos 0. Lic: GNU GPL v3. Author: Maksim Pinigin. Pre-alpha 21.08.16
+echo Pinos 0. Lic: ISC. Author: Maksim Pinigin. Pre-alpha 21.08.16
 echo Starting Pinos...
 python core.py
