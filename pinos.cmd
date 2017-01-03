21f8c181d98c5fd0fb66f1ba70a753165a806f81
Maksim Pinigin <maksim.pinigin@yahoo.com>
2017-01-03 08:26:44 +0700

    Pinos 0 Pre-alpha 21.08.16c

--- pinos.cmd
+++ pinos.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
-echo Pinos 0. Lic: ISC. Author: Maksim Pinigin. Pre-alpha 21.08.16b
+echo Pinos 0. Lic: ISC. Author: Maksim Pinigin. Pre-alpha 21.08.16c
 echo Starting Pinos...
 python C:\pinos\core.py
