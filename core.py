829199c866f273865c9ac78029dedbfc2db99a31
Maksim Pinigin <maksim@pinig.in>
2017-02-03 20:49:02 +0700

    Changed "omod math" to "math"

--- core.py
+++ core.py
@@ -132,40 +132,8 @@ while True:
                             else:
                                 print("Unknown command")
                     # ==='''
-                    # === New math ===
-                    if command[1] == "math":
-                        print("Math for Pinos by Maxim Pinigin.")
-                        while True:
-                            textmath = input("Math> ")
-                            commandmath = textmath.split(' ')
-                            if commandmath[0] == "exit":
-                                break;
-                            try:
-                                if commandmath[1] == "+":
-                                    one = int(commandmath[0])
-                                    two = int(commandmath[2])
-                                    otv = one+two
-                                    print(otv)
-                                elif commandmath[1] == "-":
-                                    one = int(commandmath[0])
-                                    two = int(commandmath[2])
-                                    otv = one-two
-                                    print(otv)
-                                elif commandmath[1] == "/":
-                                    one = int(commandmath[0])
-                                    two = int(commandmath[2])
-                                    otv = one/two
-                                    print(otv)
-                                elif commandmath[1] == "*":
-                                    one = int(commandmath[0])
-                                    two = int(commandmath[2])
-                                    otv = one*two
-                                    print(otv)
-                            except:
-                                print("Error")
-                    # ===
                     # === LinuxCMD ===
-                    elif command[1] == "linuxcmd":
+                    if command[1] == "linuxcmd":
                         print("Get username...")
                         try:
                             login = os.getlogin()
@@ -185,6 +153,36 @@ while True:
                 except:
                     print("Error!")
             # ===
+            elif command[0] == "math":
+                print("Math for Pinos by Maxim Pinigin.")
+                while True:
+                    textmath = input("Math> ")
+                    commandmath = textmath.split(' ')
+                    if commandmath[0] == "exit":
+                        break;
+                    try:
+                        if commandmath[1] == "+":
+                            one = int(commandmath[0])
+                            two = int(commandmath[2])
+                            otv = one+two
+                            print(otv)
+                        elif commandmath[1] == "-":
+                            one = int(commandmath[0])
+                            two = int(commandmath[2])
+                            otv = one-two
+                            print(otv)
+                        elif commandmath[1] == "/":
+                            one = int(commandmath[0])
+                            two = int(commandmath[2])
+                            otv = one/two
+                            print(otv)
+                        elif commandmath[1] == "*":
+                            one = int(commandmath[0])
+                            two = int(commandmath[2])
+                            otv = one*two
+                            print(otv)
+                    except:
+                        print("Error")
             elif command[0] == "cd":
                 try:
                     if os.path.exists(command[1]):
