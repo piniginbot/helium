5759646649d209ffc696c4aeb654063b734e6f18
Funtazoid <funtazoid@yandex.ru>
2018-10-14 01:33:28 +0800

    microupdate

--- helium.cmd
+++ helium.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 color 0b
-echo Helium 0.1. License: ISC. Created by Funtazoid (Oleg Asimov). Based on Pinos 21.03.17 by MaksimPinigin.
-python C:\pinos\core.py
+echo helium based on Pinos 21.03.17
+python core.py
